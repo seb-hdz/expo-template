decb7526cb752f11dbc46e4570d2951bb9402e6a
Sebastian Hidalgo <sebastian.hidalgo@unmsm.edu.pe>
2024-03-30 10:27:01 -0500

    fix(supabase.config.ts): rename envs

--- config/supabase.config.ts
+++ config/supabase.config.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.EXPO_PUBLIC_SUPABASE_URL ?? "",
+  process.env.EXPO_PUBLIC_SUPABASE_PROJECT_URL ?? "",
   process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? ""
 );
 
